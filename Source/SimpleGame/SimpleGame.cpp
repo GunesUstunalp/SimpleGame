2c09272bd7c944b5b36cd7a23ba417f062244b6f
GunesUstunalp <gunesustunalp@hotmail.com>
2022-08-02 17:43:36 +0300

    Initial

---
+++ Source/SimpleGame/SimpleGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SimpleGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SimpleGame, "SimpleGame" );
---
+++ Source/SimpleGame/SimpleGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SimpleGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIMPLEGAME_API ASimpleGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
