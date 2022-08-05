a3378865143040f8d72306fa2ed65a5a28162302
GunesUstunalp <gunesustunalp@hotmail.com>
2022-08-05 14:44:14 +0300

    Pawn Movement Added -C26

--- Source/SimpleGame/Critter.cpp
+++ Source/SimpleGame/Critter.cpp
@@ -5,6 +5,7 @@
 #include "Components/StaticMeshComponent.h"
 #include "Components/SceneComponent.h"
 #include "Camera/CameraComponent.h"
+#include "Components/InputComponent.h"
 
 // Sets default values
 ACritter::ACritter()
@@ -22,6 +23,9 @@ ACritter::ACritter()
 	Camera->SetRelativeRotation(FRotator(-45.f, 0.f, 0.f));
 
 	AutoPossessPlayer = EAutoReceiveInput::Player0;
+
+	CurrentVelocity = FVector(0.f);
+	MaxSpeed = 100.f;
 }
 
 // Called when the game starts or when spawned
@@ -36,6 +40,10 @@ void ACritter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	FVector NewLocation = GetActorLocation() + (CurrentVelocity * DeltaTime);
+	SetActorLocation(NewLocation);
+
+
 }
 
 // Called to bind functionality to input
@@ -43,5 +51,29 @@ void ACritter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &ACritter::MoveForward);
+	PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &ACritter::MoveRight);
+
+
+
+}
+
+void ACritter::MoveForward(float Value) {
+	CurrentVelocity.X = FMath::Clamp(Value, -1.f, 1.f) * MaxSpeed;
+
+	if (Value == 1) {
+		UE_LOG(LogTemp, Warning, TEXT("MovingForward"));
+	}	
+	else if(Value == -1)
+		UE_LOG(LogTemp, Warning, TEXT("MovingBackward"));
+}
+
+void ACritter::MoveRight(float Value) {
+	CurrentVelocity.Y = FMath::Clamp(Value, -1.f, 1.f) * MaxSpeed;
+	if (Value == 1) {
+		UE_LOG(LogTemp, Warning, TEXT("MovingRight"));
+	}		
+	else if (Value == -1)
+		UE_LOG(LogTemp, Warning, TEXT("MovingLeft"));
 }
 
--- Source/SimpleGame/Critter.h
+++ Source/SimpleGame/Critter.h
@@ -31,4 +31,14 @@ public:
 
 	UPROPERTY(EditAnywhere)
 	class UCameraComponent* Camera;
+
+	UPROPERTY(EditAnywhere, Category = "Pawn Movement")
+	float MaxSpeed;
+
+private:
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+
+	FVector CurrentVelocity;
+	
 };
