2a97ba46187441f42824748409a68ae163d9bec5
GunesUstunalp <gunesustunalp@hotmail.com>
2022-08-16 17:55:43 +0300

    Improvements to the SettingsMenuWidget

--- Source/SimpleGame/SettingsMenuWidget.cpp
+++ Source/SimpleGame/SettingsMenuWidget.cpp
@@ -3,18 +3,37 @@
 
 #include "SettingsMenuWidget.h"
 
+#include "Components/Button.h"
+#include "Components/TextBlock.h"
 #include "GameFramework/GameUserSettings.h"
 #include "Kismet/KismetStringLibrary.h"
 
-USettingsMenuWidget::USettingsMenuWidget(const FObjectInitializer& ObjectInitializer):Super(ObjectInitializer)
+void USettingsMenuWidget::NativeConstruct()
 {
-	AudioOptionPanel =CreateDefaultSubobject<UCanvasPanel>(TEXT("AudioOptionPanel"));
-	GraphicsOptionPanel =CreateDefaultSubobject<UCanvasPanel>("GraphicsOptionPanel");
-	GameplayOptionPanel =CreateDefaultSubobject<UCanvasPanel>("GameplayOptionPanel");
-	ControlsOptionPanel =CreateDefaultSubobject<UCanvasPanel>("ControlsOptionPanel");
+	Super::NativeConstruct();
+	AudioOptionPanel->SetVisibility(ESlateVisibility::Visible); // Show the audio panel by default
+	GraphicsOptionPanel->SetVisibility(ESlateVisibility::Hidden);
+	GameplayOptionPanel->SetVisibility(ESlateVisibility::Hidden);
+	ControlsOptionPanel->SetVisibility(ESlateVisibility::Hidden);
+
+	//At the editor the Render Transforms of the option menu panels are set different from each other to easily edit them. If not they would overlap on each other
+	//But we have to set the Render Transforms of the panels to the default value before executing the code
+	//This is the reason the code piece below exists, to reset the Render Transforms of those panels
+	const FWidgetTransform PanelTransform(FVector2D(0.f,0.f),FVector2D(1.f,1.f),FVector2D(0.f,0.f),0.f); //Default transform
+	AudioOptionPanel->SetRenderTransform(PanelTransform);
+	GraphicsOptionPanel->SetRenderTransform(PanelTransform);
+	GameplayOptionPanel->SetRenderTransform(PanelTransform);
+	ControlsOptionPanel->SetRenderTransform(PanelTransform);
+
+	AudioOptionButton->OnClicked.AddDynamic(this, &USettingsMenuWidget::OnAudioOptionButtonClicked);
+	GraphicsOptionButton->OnClicked.AddDynamic(this, &USettingsMenuWidget::OnGraphicsOptionButtonClicked);
+	GameplayOptionButton->OnClicked.AddDynamic(this, &USettingsMenuWidget::OnGameplayOptionButtonClicked);
+	ControlsOptionButton->OnClicked.AddDynamic(this, &USettingsMenuWidget::OnControlsOptionButtonClicked);
+
+	WindowModeComboBox
 }
 
-void USettingsMenuWidget::SetResolution(FString Resolution)
+void USettingsMenuWidget::SetResolution(FString Resolution) //TODO: TIDY-UP
 {
 	FString LeftS, RightS;
 	Resolution.Split("x", &LeftS, &RightS);
@@ -22,4 +41,51 @@ void USettingsMenuWidget::SetResolution(FString Resolution)
 	GEngine->GameUserSettings->SetScreenResolution(FIntPoint(UKismetStringLibrary::Conv_StringToInt(LeftS), UKismetStringLibrary::Conv_StringToInt(RightS)));
 	GEngine->GameUserSettings->ApplyResolutionSettings(false);
 	UE_LOG(LogTemp, Warning, TEXT("Set : %i, %i"), GEngine->GameUserSettings->GetScreenResolution().X, GEngine->GameUserSettings->GetScreenResolution().Y);
+}
+
+void USettingsMenuWidget::SetWindowMode(FString WindowMode)
+{
+	if(WindowMode.Equals("Fullscreen"))
+		GEngine->GameUserSettings->SetFullscreenMode(EWindowMode::Fullscreen);
+	else if(WindowMode.Equals("Windowed"))
+		GEngine->GameUserSettings->SetFullscreenMode(EWindowMode::Windowed);
+	else if(WindowMode.Equals("Borderless"))
+		GEngine->GameUserSettings->SetFullscreenMode(EWindowMode::WindowedFullscreen);
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("ERROR! Unknown WindowMode!!!"));
+	}
+}
+
+
+void USettingsMenuWidget::OnAudioOptionButtonClicked()
+{
+	AudioOptionPanel->SetVisibility(ESlateVisibility::Visible);
+	GraphicsOptionPanel->SetVisibility(ESlateVisibility::Hidden);
+	GameplayOptionPanel->SetVisibility(ESlateVisibility::Hidden);
+	ControlsOptionPanel->SetVisibility(ESlateVisibility::Hidden);
+}
+
+void USettingsMenuWidget::OnGraphicsOptionButtonClicked()
+{
+	AudioOptionPanel->SetVisibility(ESlateVisibility::Hidden);
+	GraphicsOptionPanel->SetVisibility(ESlateVisibility::Visible);
+	GameplayOptionPanel->SetVisibility(ESlateVisibility::Hidden);
+	ControlsOptionPanel->SetVisibility(ESlateVisibility::Hidden);
+}
+
+void USettingsMenuWidget::OnGameplayOptionButtonClicked()
+{
+	AudioOptionPanel->SetVisibility(ESlateVisibility::Hidden);
+	GraphicsOptionPanel->SetVisibility(ESlateVisibility::Hidden);
+	GameplayOptionPanel->SetVisibility(ESlateVisibility::Visible);
+	ControlsOptionPanel->SetVisibility(ESlateVisibility::Hidden);
+}
+
+void USettingsMenuWidget::OnControlsOptionButtonClicked()
+{
+	AudioOptionPanel->SetVisibility(ESlateVisibility::Hidden);
+	GraphicsOptionPanel->SetVisibility(ESlateVisibility::Hidden);
+	GameplayOptionPanel->SetVisibility(ESlateVisibility::Hidden);
+	ControlsOptionPanel->SetVisibility(ESlateVisibility::Visible);
 }
--- Source/SimpleGame/SettingsMenuWidget.h
+++ Source/SimpleGame/SettingsMenuWidget.h
@@ -4,7 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Components/Button.h"
 #include "Components/CanvasPanel.h"
+#include "Components/ComboBox.h"
 #include "SettingsMenuWidget.generated.h"
 
 /**
@@ -14,20 +16,68 @@ UCLASS()
 class SIMPLEGAME_API USettingsMenuWidget : public UUserWidget
 {
 	GENERATED_BODY()
-
-	
 public:
-	USettingsMenuWidget(const FObjectInitializer& ObjectInitializer);
+	void NativeConstruct() override; //constructor for the class
 	
-	UPROPERTY(BlueprintReadWrite, Category= Panels)
+	UPROPERTY(BlueprintReadWrite, Category= Panels, meta=(BindWidget))
     UCanvasPanel* AudioOptionPanel;
-	UPROPERTY(BlueprintReadWrite, Category= Panels)
+	UPROPERTY(BlueprintReadWrite, Category= Panels, meta=(BindWidget))
     UCanvasPanel* GraphicsOptionPanel;
-	UPROPERTY(BlueprintReadWrite, Category= Panels)
+	UPROPERTY(BlueprintReadWrite, Category= Panels, meta=(BindWidget))
     UCanvasPanel* GameplayOptionPanel;
-	UPROPERTY(BlueprintReadWrite, Category= Panels)
+	UPROPERTY(BlueprintReadWrite, Category= Panels, meta=(BindWidget))
     UCanvasPanel* ControlsOptionPanel;
-    
+
+	//Menu Selection Buttons on the left side of the screen
+	UPROPERTY(BlueprintReadWrite, Category= Buttons, meta=(BindWidget))
+	UButton* AudioOptionButton;
+	UPROPERTY(BlueprintReadWrite, Category= Buttons, meta=(BindWidget))
+	UButton* GraphicsOptionButton;
+	UPROPERTY(BlueprintReadWrite, Category= Buttons, meta=(BindWidget))
+	UButton* GameplayOptionButton;
+	UPROPERTY(BlueprintReadWrite, Category= Buttons, meta=(BindWidget))
+	UButton* ControlsOptionButton;
+
+	UFUNCTION()
+	void OnAudioOptionButtonClicked();
+	UFUNCTION()
+	void OnGraphicsOptionButtonClicked();
+	UFUNCTION()
+	void OnGameplayOptionButtonClicked();
+	UFUNCTION()
+	void OnControlsOptionButtonClicked();
+
+	//Logic for the GraphicsOptionPanel
+	UPROPERTY(BlueprintReadWrite, Category= GraphicsOption, meta=(BindWidget))
+	UComboBox* WindowModeComboBox;
+
+	UPROPERTY(BlueprintReadWrite, Category= GraphicsOption, meta=(BindWidget))
+	UComboBox* ResolutionComboBox;
+
+	UPROPERTY(BlueprintReadWrite, Category= GraphicsOption, meta=(BindWidget))
+	UComboBox* QualityPresetComboBox;
+	
 	UFUNCTION(BlueprintCallable)
 	void SetResolution(FString Resolution);
+
+	UFUNCTION(BlueprintCallable)
+	void SetWindowMode(FString WindowMode);
+
+	UFUNCTION(BlueprintCallable)
+	void SetQualityPreset(FString QualityPreset);
+
+	UFUNCTION(BlueprintCallable)
+	void SetBrightness(FString Brightness);
+
+	UFUNCTION(BlueprintCallable)
+	void SetGamma(FString Gamma);
+
+	//Logic for the AudioOptionPanel
+	//TODO
+
+	//Logic for the GameplayOptionPanel
+	//TODO
+
+	//Logic for the ControlsOptionPanel
+	//TODO
 };
